8668b6328b1f0040a06cff511c8782e64e0e8066
Subeg Aryal <subeg7@gmail.com>
2020-05-06 09:06:28 +0545

    token generated

--- routes/routes.js
+++ routes/routes.js
@@ -7,11 +7,15 @@ const router = express.Router();
 //When the user sends a post request to this route, passport authenticates the user based on the
 //middleware created previously
 router.post('/signup', passport.authenticate('signup', { session: false }), async (req, res, next) => {
+    console.log("here");
     res.json({
         message: 'Signup successful',
         user: req.user
     });
 });
+
+router.get('/test', (req, res) => res.send('Hello World with Express'));
+
 
 router.post('/login', async (req, res, next) => {
     passport.authenticate('login', async (err, user, info) => {
